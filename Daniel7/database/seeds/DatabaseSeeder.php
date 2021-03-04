b59ca443cfd364d008ff26c4064c4d472f5efa97
satishdodia <sb.dodia@gmail.com>
2021-03-04 11:15:50 +0530

    new demo
    latest code with version downgrade

---
+++ Daniel7/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Daniel',
+            'email' => 'daniel@gmail.com',
+            'password' => bcrypt(123456)
+        ]);
+    }
+}
